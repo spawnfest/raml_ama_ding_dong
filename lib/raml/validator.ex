d82460955d1c3086c68da148c26492d508829563
claytonflesher <claytonflesher@gmail.com>
2018-11-24 14:56:21 -0600

    max_properties validation and test

--- lib/raml/validator.ex
+++ lib/raml/validator.ex
@@ -1,5 +1,26 @@
 defmodule RAML.Validator do
   def validate(fields, declaration, types) do
-    :ok
+    type = types
+    |> Enum.filter(fn type -> type.name == declaration end)
+    |> List.first
+
+    with :ok <- validate_max_properties(fields, type.max_properties) do
+      {:ok, fields}
+    end
   end
-end
+
+  def validate_max_properties(fields, max) when is_integer(max) do
+    actual = fields
+    |> Map.keys
+    |> length
+
+    case actual <= max do
+      true  -> :ok
+      false -> {:error, :max_properties}
+    end
+  end
+
+  def validate_max_properties(fields, _) do
+    {:ok, fields}
+  end
+ end
