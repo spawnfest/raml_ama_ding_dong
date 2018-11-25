aead00766d937cf43df07f31845e4db9ebc42192
James Edward Gray II <james@graysoftinc.com>
2018-11-25 16:25:42 -0600

    Fixing the tests.

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -81,7 +81,7 @@ defmodule RAMLParserTest do
     parsed = Parser.parse(fixture("raml_redirects.raml"))
 
     redirects = Enum.find(parsed.resources, fn r -> r.path == "/redirects" end)
-    assert redirects.methods.put.query_string == "Redirect"
+    assert redirects.methods.put.query_string.type == "Redirect"
     assert redirects.methods.put.responses["200"].body == "ShortURL"
 
     redirect = Enum.find(parsed.types, fn type -> type.name == "Redirect" end)
